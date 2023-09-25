63e93666d33bdbcccbfd204a3ae1c7e0d630ee0a
unknown <bazanjuanjordan@gmail.com>
2023-09-25 17:28:01 -0300

    Se crea pagina de incio

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-
+import '../css/HomePage.css'
 import React from 'react'
 import CardsC from '../components/CardsC'
 import { Container, Row } from 'react-bootstrap'
@@ -6,11 +6,29 @@ import { Container, Row } from 'react-bootstrap'
 const HomePage = () => {
   return (
     <>
-    <Container>
-      <Row>
-        <CardsC/>
-      </Row>
-    </Container>
+      <Container className='main'>
+        <Row>
+          <hr />
+          <h1 className='text-center'>Hotel Nossa</h1>
+          <hr />
+          <h4 className='text-center'>Hotel Nossa Tucuman, Tafi del Valle | Argentina </h4>
+          <hr />
+          <p className='start'>El Nossa Tafi del Valle de 5 estrellas presenta una arquitectura clásica y ofrece jardines hermosos y vistas
+            panorámicas en un ambiente tranquilo.
+            El club de bienestar y spa tiene pileta cubierta, gimnasio, sauna y solárium. Hay masajes, shiatsu, entrenador personal y 
+            otros servicios disponibles a pedido.
+            El Nossa dispone de 2 bares y de 2 restaurantes famosos por su sabrosa carne a la parrilla y platos mediterráneos.
+            El hotel se encuentra en el tradicional circuito del Cerro el Palao, cerca de los artesanos, fabricas de quesos.</p>
+            <hr />
+          <h4>Fotos del Nossa </h4>
+          <CardsC />
+          <hr />
+          <h4>Habitaciones</h4>
+          <CardsC />
+          <h4>Servicios</h4>
+          <CardsC />
+        </Row>
+      </Container>
     </>
   )
 }
