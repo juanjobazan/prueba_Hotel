68d4dd0903fdcb6faad4d83cd7c999a3e64093ab
unknown <bazanjuanjordan@gmail.com>
2023-09-26 21:41:22 -0300

    se crea pagina Admin

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,10 +1,109 @@
-import React from 'react'
+import '../css/User.css'
+import Table from 'react-bootstrap/Table';
 
 const AdminPage = () => {
   return (
-    <div>
-      AdminPage
-    </div>
+    <>
+      <div className="container main">
+        <div>
+          <h1>ABM Usuarios</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Usuario</th>
+                <th>Correo</th>
+                <th>Roll</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@mdo</td>
+              </tr>
+
+
+            </tbody>
+          </Table>
+        </div>
+        <hr />
+        <div>
+          <h1>ABM Habitaciones</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Detalle</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@mdo</td>
+              </tr>
+            
+            </tbody>
+          </Table>
+        </div>
+        <hr />
+        <div>
+          <h1>ABM Servicios</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Detalle</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@mdo</td>
+              </tr>
+            
+            </tbody>
+          </Table>
+        </div>
+        <hr />
+      </div>
+    </>
   )
 }
 
