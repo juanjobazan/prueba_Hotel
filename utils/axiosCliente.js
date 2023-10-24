81a866176311e43c72724bd5381ff32c08297626
unknown <bazanjuanjordan@gmail.com>
2023-10-24 18:23:48 -0300

    se modifica pagina admin habi

--- utils/axiosCliente.js
+++ utils/axiosCliente.js
@@ -4,8 +4,8 @@ import axios  from "axios";
 const token = JSON.parse(localStorage.getItem('token'))
 
 const clienteAxios = axios.create({
-
-    baseURL: `${import.meta.env.VITE_URL_BACK_DEPLOY}/api` 
+    baseURL:'localhost:3000/api'
+    //baseURL: `${import.meta.env.VITE_URL_BACK_DEPLOY}/api` 
    
 
 })
